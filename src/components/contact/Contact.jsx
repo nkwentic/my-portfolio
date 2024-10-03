6ce2d0389a42010bcc259a23c125191ebf738ce7
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-10-03 02:50:01 -0400

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,22 +15,22 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>lol@gmail.com</h5>
+            <h5>afroinstituteoftechnology$gmail.com</h5>
             <a href="mailto:lol@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
             <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
-            <h5>lollol@gmail.com</h5>
+            <h5>afroinstituteoftechnology$gmail.com</h5>
             <a href="https://www.google.com/" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
             <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
-            <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone=+919354041804" target="_blank" rel="noreferrer">Send a message</a>
+            <h5>+12406431098</h5>
+            <a href="https://api.whatsapp.com/send?phone=+12406431098" target="_blank" rel="noreferrer">Send a message</a>
           </article>
         </div>
         <form action="">
