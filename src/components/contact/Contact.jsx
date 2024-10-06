b9d36a950b07e36cabb9c8bc2af096dffafea38c
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-10-06 13:00:26 -0400

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,21 +8,21 @@ const Contact = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2>Contact Ekangaki</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>afroinstituteoftechnology$gmail.com</h5>
+            <h5>afroinstituteoftechnology@gmail.com</h5>
             <a href="mailto:lol@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
             <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
-            <h5>afroinstituteoftechnology$gmail.com</h5>
+            <h5>afroinstituteoftechnology@gmail.com</h5>
             <a href="https://www.google.com/" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
