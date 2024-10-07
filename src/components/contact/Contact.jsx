592bb3950340cb117dc698255dc7feb48216a656
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-10-07 12:41:33 -0400

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,14 +15,14 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>afroinstituteoftechnology@gmail.com</h5>
+            <h5>afrotech@gmail.com</h5>
             <a href="mailto:lol@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
             <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
-            <h5>afroinstituteoftechnology@gmail.com</h5>
+            <h5>afrotech@gmail.com</h5>
             <a href="https://www.google.com/" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
