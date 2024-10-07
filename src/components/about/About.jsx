2318f0b8359e91c6736cd56dcd0dd44cc28de413
E Njang <104914526+Ekangaki@users.noreply.github.com>
2024-10-07 12:38:17 -0400

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,13 +23,13 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>9+ Years Working</small>
             </article>
 
             <article className="about__card">
               <FiUsers className="about__icon" />
-              <h5>Clients</h5>
-              <small>200+ Clients Worldwide</small>
+              <h5>Core Interest</h5>
+              <small>Africans and Worldwide</small>
             </article>
 
             <article className="about__card">
@@ -40,9 +40,10 @@ const About = () => {
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Accusantium neque veritatis temporibus dolor soluta cumque facere
-            dolorem distinctio a est?
+           Ekangaki George Gedeon Njang is a seasoned Networking, Cloud, and DevOps Engineer with over 9 years of experience in designing, building, 
+            and automating both on-premises and cloud environments. Passionate about empowering Africans, 
+            he is dedicated to teaching IT skills to individuals with no prior technical background, making complex concepts accessible and relatable. 
+            Through his work, he strives to create opportunities for self-reliance and growth, driving positive change in African communities
           </p>
 
           <a href="#contact" className="btn btn-primary">
